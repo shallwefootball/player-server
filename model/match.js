ef83e31106aebd108935e593108813969a441258
amos <amos@shallwefootball.com>
2016-11-02 01:00:20 +0900

    match 쿼리 수정

--- model/match.js
+++ model/match.js
@@ -2,5 +2,52 @@ const conn = require('./conn')
 
 exports.select = leagueId => {
 
-  return conn(`select * from \`match\` where leagueId = ?`, leagueId)
+  return conn(
+    `select
+      concat("#", @RNUM := @RNUM + 1) AS rownum,
+      m.matchId,
+      m.matchName,
+      m.kickoffTime,
+      m.homeClubId,
+      (
+        select (select teamId from team t where t.teamId = c.teamId)teamName
+        from club c
+        where m.homeClubId = c.clubId
+      )
+      homeTeamId,
+      (
+        select (select teamName from team t where t.teamId = c.teamId)teamName
+        from club c
+        where m.homeClubId = c.clubId
+      )
+      homeClubName,
+      m.awayClubId,
+      (
+        select (select teamName from team t where t.teamId = c.teamId)teamName
+        from club c
+        where m.awayClubId = c.clubId
+      )
+      awayClubName,
+      (
+        select (select teamId from team t where t.teamId = c.teamId)teamName
+        from club c
+        where m.awayClubId = c.clubId
+      )
+      awayTeamId,
+      if (
+        m.kickoffTime < now() and isnull(m.homeScore), 0, m.homeScore
+      )
+      homeScore,
+      if (
+        m.kickoffTime < now() and isnull(m.awayScore), 0, m.awayScore
+      )
+      awayScore,
+      m.leagueId,
+      m.stadium,
+      m.note,
+      m.link
+    from \`match\` m, ( SELECT @RNUM := 0 ) R
+    where m.leagueId = ? order by kickoffTime`,
+    leagueId
+  )
 }
