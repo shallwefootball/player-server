6684eabaa2a002ce54b01dc7daf2a56fa7f9e304
amos <amos@shallwefootball.com>
2016-12-04 22:07:36 +0900

    test select spec추가

--- __tests__/team.model.spec.js
+++ __tests__/team.model.spec.js
@@ -1,3 +1,4 @@
+const _ = require('lodash')
 const teamModel = require('../model/team')
 
 describe('teamModel', () => {
@@ -5,4 +6,22 @@ describe('teamModel', () => {
   it('api의 갯수는 2 이다.', () => (expect(keys.length).toEqual(2)))
   it('select 가 포함 되어있다.', () => (expect(keys).toContain('select')))
   it('selectOne 가 포함 되어있다.', () => (expect(keys).toContain('selectOne')))
+})
+
+
+describe('teamModel.select', () => {
+  it('배열을 리턴한다.',  () => {
+    return teamModel.select()
+    .then(clubs => {
+      expect(_.isArray(clubs)).toBeTruthy()
+    })
+  })
+  it('각각 인덱스는 객체리터럴이다.',  () => {
+    return teamModel.select()
+    .then(clubs => {
+      expect(clubs.every(match => {
+        return _.isObject(match)
+      })).toBeTruthy()
+    })
+  })
 })
